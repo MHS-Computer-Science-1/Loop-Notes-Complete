5192f7f7933e4666009afd524b986cdba5fc8516
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2024-09-13 09:24:20 -0400

    Add files via upload

---
+++ Loop Notes/Loop Notes/Form1.Designer.vb
@@ -0,0 +1,210 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ListBox1 = New ListBox()
+        Button1 = New Button()
+        Label1 = New Label()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Label2 = New Label()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        SuspendLayout()
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 21
+        ListBox1.Location = New Point(262, 43)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(179, 319)
+        ListBox1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(12, 50)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(202, 23)
+        Button1.TabIndex = 1
+        Button1.Text = "Display 1-5 the LONG way"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(12, 17)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(127, 30)
+        Label1.TabIndex = 2
+        Label1.Text = "FOR LOOPS"
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(11, 79)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(202, 23)
+        Button2.TabIndex = 3
+        Button2.Text = "Display 1-5 the LOOP way"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(12, 108)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(202, 23)
+        Button3.TabIndex = 4
+        Button3.Text = "Try It: Display -10 to 10"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(12, 137)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(202, 23)
+        Button4.TabIndex = 5
+        Button4.Text = "Count from 2-100 by 2s"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(12, 166)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(202, 23)
+        Button5.TabIndex = 6
+        Button5.Text = "Try It: Countdown"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(12, 219)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(189, 30)
+        Label2.TabIndex = 7
+        Label2.Text = "DO WHILE LOOPS"
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(12, 252)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(202, 23)
+        Button6.TabIndex = 8
+        Button6.Text = "Display 1-5 the LOOP way"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(12, 281)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(202, 23)
+        Button7.TabIndex = 9
+        Button7.Text = "Try It: Display -10 to 10"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(12, 339)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(202, 23)
+        Button8.TabIndex = 10
+        Button8.Text = "Guessing Game"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Location = New Point(12, 310)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(202, 23)
+        Button9.TabIndex = 11
+        Button9.Text = "Boost User Self-Esteem"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.Location = New Point(554, 186)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(202, 23)
+        Button10.TabIndex = 12
+        Button10.Text = "Clear the ListBox"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button11
+        ' 
+        Button11.Location = New Point(572, 227)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(75, 23)
+        Button11.TabIndex = 13
+        Button11.Text = "Button11"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.InactiveCaption
+        ClientSize = New Size(464, 396)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Label2)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        Controls.Add(ListBox1)
+        Name = "Form1"
+        Text = "LOOP NOTES"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+End Class
---
+++ Loop Notes/Loop Notes/Form1.vb
@@ -0,0 +1,154 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Add 1-5 to the ListBox the LONG WAY
+        ListBox1.Items.Add(1)
+        ListBox1.Items.Add(2)
+        ListBox1.Items.Add(3)
+        ListBox1.Items.Add(4)
+        ListBox1.Items.Add(5)
+    End Sub
+
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Add 1-5 to the ListBox the LOOP WAY
+
+        'Loops are used to automatically repeat code
+        'Our loop will use the i variable to
+        'count from 1 to 5
+        Dim i As Integer
+
+        For i = 1 To 5
+            ListBox1.Items.Add(i)
+        Next
+    End Sub
+
+
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'Display -10 to 10
+        Dim i As Integer
+
+        For i = -10 To 10
+            ListBox1.Items.Add(i)
+        Next
+
+
+
+    End Sub
+
+
+
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim i As Integer
+
+        'A step command can be used to make the loop
+        'count differently
+        'Step 2, means count up by 2s
+        For i = 2 To 100 Step 2
+            ListBox1.Items.Add(i)
+        Next
+    End Sub
+
+
+
+
+
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        'Display a NASA countdown from 10 to 1 in the ListBox
+        'At the end, display "BLASTOFF!"
+        Dim i As Integer
+
+        For i = 10 To 1 Step -1
+            ListBox1.Items.Add(i)
+        Next
+        ListBox1.Items.Add("Blastoff!")
+
+
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+        'DO WHILE loops can do the same things as for loops
+        Dim i As Integer
+        i = 1
+        'DO WHILE loops repeat until the Boolean expression
+        'is FALSE
+        Do While i <= 5
+            ListBox1.Items.Add(i)
+            i = i + 1 'add 1 so that i moves closer to 5
+        Loop
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        'Display -10 to 10
+
+        Dim i As Integer
+        i = -10
+
+        Do While i <= 10
+            ListBox1.Items.Add(i)
+            i = i + 1
+        Loop
+
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        'DO WHILE Loops can do more than count
+
+        Dim r As New Random
+        Dim secret As Integer
+        secret = r.Next(1, 11)
+
+        Dim guess As Integer
+        'Loop while the player has NOT guessed correctly
+        Do While guess <> secret
+            'Ask the player to guess
+            guess = InputBox("Enter your guess")
+        Loop
+
+        MsgBox("You got it right!")
+
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Dim userinput As String
+        userinput = InputBox("Do you want to feel better?")
+
+        Do While userinput = "Yes" Or userinput = "yes"
+            MsgBox("You are great!!!") 'Boost user self-esteem
+            'Ask again
+            userinput = InputBox("Do you want to feel better?")
+        Loop
+
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        ListBox1.Items.Clear()
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Dim r As New Random
+        Dim mine, userinput As Integer
+
+        mine = r.Next(1, 6)
+
+        MsgBox("Welcome to Minesweeper!")
+        MsgBox("There are 5 spaces. One has a mine.")
+        MsgBox("Guess until you explode.")
+
+        Do While userinput <> mine
+            userinput = InputBox("Guess!")
+        Loop
+
+        MsgBox("Boom!")
+
+    End Sub
+End Class
